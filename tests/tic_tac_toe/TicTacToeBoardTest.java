b5621fa1c46a2504bcb582f76e30dba4352c0f91
timostrating <timostrating@home.nl>
2019-02-17 01:05:16 +0100

    tested out tests

---
+++ tests/tic_tac_toe/TicTacToeBoardTest.java
@@ -0,0 +1,27 @@
+package tic_tac_toe;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeBoardTest {
+
+    TicTacToeBoard board;
+
+    @BeforeEach
+    void BeforeEach() {
+        board = new TicTacToeBoard();
+    }
+
+    @Test
+    void playMove() {
+        board.playMove(0, 0, 1);
+        board.playMove(0, 2, 2);
+        board.playMove(1, 2, 1);
+        board.playMove(1, 1, 2);
+        board.playMove(2, 2, 1);
+        board.playMove(2, 0, 2);
+        assert board.getGameState() == TicTacToeBoard.GameState.Player2Wins;
+    }
+}
