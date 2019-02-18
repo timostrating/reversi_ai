60d2b880ed1ebadc771a1c8ae31452672fc6f5fa
timostrating <timostrating@home.nl>
2019-02-18 17:24:42 +0100

    made the reversi rules work

--- src/reversi/ReversiBoard.java
+++ src/reversi/ReversiBoard.java
@@ -3,7 +3,11 @@ package reversi;
 public class ReversiBoard {
 
     /* Enums are classes and should follow the conventions for classes. Instances of an enum are constants and should follow the conventions for constants.https://stackoverflow.com/a/3069863 */
-    public enum CellState {EMPTY, X, O; public static CellState fromNr(int nr) {return CellState.values()[nr];} }
+    public enum CellState {
+        EMPTY, X, O;
+        public static CellState fromNr(int nr) {return CellState.values()[nr];}
+        public static CellState opponentFromNr(int nr) {return CellState.values()[nr%2+1];}
+    }
     public enum GameState {PLAYING, DRAW, PLAYER_1_WINS, PLAYER_2_WINS}
     public enum BoardDirection {
         LEFT_UP  (-1,  1),  UP  (0,  1),    RIGHT_UP  (1,  1),
@@ -23,33 +27,92 @@ public class ReversiBoard {
     public ReversiBoard(int boardSize) {
         board = new CellState[boardSize][boardSize];
         this.reset();
+    }
+
+    // TODO: This accounts for all boards
+    public void reset() {
+        int boardSize = getBoardSize();
+        for (int i=0; i<boardSize; i++)
+            for (int j=0; j<boardSize; j++)
+                board[i][j] = CellState.EMPTY;
+
         board[boardSize/2 -1][boardSize/2 -1] = CellState.fromNr(1);
         board[boardSize/2 -1][boardSize/2   ] = CellState.fromNr(2);
         board[boardSize/2   ][boardSize/2   ] = CellState.fromNr(1);
         board[boardSize/2   ][boardSize/2 -1] = CellState.fromNr(2);
     }
-
-    // TODO: This accounts for all boards
-    public void reset() {
-        for (int i=0; i<getBoardSize(); i++)
-            for (int j=0; j<getBoardSize(); j++)
-                board[i][j] = CellState.EMPTY;
+    public CellState[][] getBoard() { return board; }
+    public int getBoardSize() { return board.length; }
+    private boolean isInBounds(int x, int y) {
+        return x >= 0 && x < getBoardSize() &&
+               y >= 0 && y < getBoardSize();
     }
+
+
+
 
     public GameState getGameState() {
-        return GameState.PLAYING; // TODO:
+        if (movesLeft())
+            return GameState.PLAYING;
+
+        int score_X = 0;
+        int score_O = 0;
+
+        for (int y=0; y<getBoardSize(); y++) {
+            for (int x = 0; x < getBoardSize(); x++) {
+                if (board[y][x] == CellState.X)
+                    score_X++;
+                if (board[y][x] == CellState.O)
+                    score_O++;
+            }
+        }
+
+        if (score_X == score_O)
+            return GameState.DRAW;
+        if (score_X < score_O)
+            return GameState.PLAYER_1_WINS;
+        return GameState.PLAYER_2_WINS;
     }
 
+    private boolean movesLeft() {
+        for (int y=0; y<getBoardSize(); y++)
+            for (int x=0; x<getBoardSize(); x++)
+                if (board[y][x] == CellState.EMPTY)
+                    return true;
 
-    public CellState[][] getBoard() { return board; } // TODO:
-    public int getBoardSize() { return board.length; }
-
+        return false;
+    }
 
     public boolean playMove(int x, int y, int playerNr) {
         if (isValidMove(x, y, playerNr)) {
-            board[y][x] = CellState.fromNr(playerNr);
-            // TODO: ...
-            return true;
+            boolean flippedOpponant = false;
+            for (BoardDirection dir : BoardDirection.values()) {
+                int n = 1;
+                boolean hasVisitedOpponent = false;
+                int newX = x + n*dir.changeX;
+                int newY = y + n*dir.changeY;
+                while (isInBounds(newX, newY) && board[newY][newX] != CellState.EMPTY ) {
+                    if (board[newY][newX] == CellState.fromNr(playerNr)) {
+                        if (hasVisitedOpponent) {
+                            flippedOpponant = true;
+//                            System.out.printf("%s: (%d,%d) -> (%d,%d)\n", "BACKTRACK",newX, newY, x, y);
+                            while (n-->1)
+                                board[y + n*dir.changeY][x + n*dir.changeX] = CellState.fromNr(playerNr);
+                        }
+                        break;
+                    } else if (board[newY][newX] == CellState.opponentFromNr(playerNr)) {
+                        hasVisitedOpponent = true;
+                        n++;
+                        newX = x + n*dir.changeX;
+                        newY = y + n*dir.changeY;
+                    }
+                }
+            }
+
+            if (flippedOpponant) {
+                board[y][x] = CellState.fromNr(playerNr);
+                return true;
+            }
         }
         return false;
     }
@@ -58,16 +121,12 @@ public class ReversiBoard {
         if (!isInBounds(x, y) || board[y][x] != CellState.EMPTY)
             return false;
 
-        for(BoardDirection pos : BoardDirection.values())
-            if (isInBounds(x + pos.changeX, y + pos.changeY))
-                if (board[y + pos.changeY][x + pos.changeX] == CellState.fromNr(playerNr%2+1))
+        for(BoardDirection dir : BoardDirection.values())
+            if (isInBounds(x + dir.changeX, y + dir.changeY))
+                if (board[y + dir.changeY][x + dir.changeX] == CellState.opponentFromNr(playerNr))
                     return true;
 
         return false;
-    }
-    private boolean isInBounds(int x, int y) {
-        return x >= 0 && x < getBoardSize() &&
-               y >= 0 && y < getBoardSize();
     }
 
     @Override
--- tests/reversi/ReversiBoardTest.java
+++ tests/reversi/ReversiBoardTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 class ReversiBoardTest {
 
@@ -18,18 +18,43 @@ class ReversiBoardTest {
     @AfterEach
     void AfterEach() {
         System.out.println(board);
+        board.reset();
     }
+
+    @Test void playExampleMoves1() {
+        assert board.playMove(4, 2, 1);
+        assert board.playMove(5, 2, 2);
+        assert board.playMove(6, 2, 1);
+        assert board.playMove(6, 1, 2);
+        assert board.playMove(6, 0, 1);
+        assert board.playMove(7, 0, 2);
+        assert board.playMove(5, 3, 1);
+        assert board.playMove(5, 4, 2);
+    }
+    //   0 1 2 3 4 5 6 7
+    // 0 - - - - - - X O
+    // 1 - - - - - - O -
+    // 2 - - - - X O X -
+    // 3 - - - X X O - -
+    // 4 - - - O O O - -
+    // 5 - - - - - - - -
+    // 6 - - - - - - - -
+    // 7 - - - - - - - -
+
 
     @Test
     void playFirstMove() {
-        ArrayList legalMoves = new ArrayList<int[]>();
-        legalMoves.add(new int[]{2, 4});
-        legalMoves.add(new int[]{3, 4});
-        legalMoves.add(new int[]{4, 2});
-        legalMoves.add(new int[]{3, 4});
+        HashMap<Integer, Boolean> legalMoves = new HashMap<>();
+        legalMoves.put(24, true);
+        legalMoves.put(35, true);
+        legalMoves.put(42, true);
+        legalMoves.put(53, true);
 
-        for (int y = 0; y < board.getBoardSize(); y++)
-            for (int x = 0; x < board.getBoardSize(); x++)
-                assert (legalMoves.contains(new int[]{x, y}) == board.playMove(x, y, 1));
+        for (int y = 0; y < board.getBoardSize(); y++) {
+            for (int x = 0; x < board.getBoardSize(); x++) {
+                assert legalMoves.containsKey(Integer.parseInt(""+x+""+y)) == board.playMove(x, y, 1);
+                board.reset();
+            }
+        }
     }
 }
