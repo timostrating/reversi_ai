2e7b94b2e81d3ccd1bbf5fdbcd0f0c358a2cb9eb
Kevin Jersak <KevinJersak@hotmail.com>
2019-04-01 15:46:14 +0200

    netwerk toegevoegd

---
+++ src/netwerk/Main.java
@@ -0,0 +1,11 @@
+package netwerk;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = new Connection();
+        connection.connection();
+    }
+}
