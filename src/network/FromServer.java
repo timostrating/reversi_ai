79e573a6b8a7220818e56aba0f5dd6c39ff064c2
Kevin Jersak <KevinJersak@hotmail.com>
2019-04-05 12:59:11 +0200

    Fromserver terug gezet

--- src/network/FromServer.java
+++ src/network/FromServer.java
@@ -35,7 +35,7 @@ public class FromServer implements Runnable {
         String[] keyValuePairs = input.split(",");
         for(String pair: keyValuePairs){
             String[] key = pair.split(": ");
-            map.put(key[0], key[1]);
+            map.put(key[0].trim(), key[1].trim());
         }
         return map;
     }
